dae7dd406dc2229b77eb325f77a727e2264cd319
Ish <ish@illuminatedbits.com>
2018-05-08 16:54:56 -0700

    initial commit

---
+++ ScrollingStacks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScrollingStacks
+//
+//  Created by Ish on 5/8/18.
+//  Copyright © 2018 Illuminated Bits LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
